21d20006c44b80e5b89f75eb464c19a24aab4f34
Marco Lau <lau.career@gmail.com>
2015-06-21 02:14:24 -0400

    Fix query bug in database script

--- models/database.js
+++ models/database.js
@@ -3,5 +3,5 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/to
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text varchar(40) not null, complete BOOLEAN');
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text varchar(40) not null, complete BOOLEAN)');
 query.on('end', function() { client.end(); });
