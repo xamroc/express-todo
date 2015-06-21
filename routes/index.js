759ccd059fb7e71b5fbb28ee5ebf86edea4a7e59
Marco Lau <lau.career@gmail.com>
2015-06-21 05:40:48 -0400

    Add create route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost/todo';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +9,39 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.post('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  // Grab data from http request
+  var data = {text: req.body.text, complete: false};
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+
+    console.log(client);
+
+    // SQL Query > Insert Data
+    client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    // Handle errors
+    if (err) {
+      console.log(err);
+    }
+  });
+});
