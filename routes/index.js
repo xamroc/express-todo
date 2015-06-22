716c7713a21672830651c5b56a1f66fada45083f
Marco Lau <lau.career@gmail.com>
2015-06-21 20:35:37 -0400

    Add RUD to todos

--- routes/index.js
+++ routes/index.js
@@ -45,3 +45,102 @@ router.post('/api/v1/todos', function(req, res) {
     }
   });
 });
+
+router.get('/api/v1/todos', function(req, res) {
+
+  var results = [];
+
+  // Get a connection client from the connection pool
+  pg.connect(connectionString, function(err, client, done){
+
+    // SQL query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    // Handle errors
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+router.put('/api/v1/todos/:todo_id', function(req, res) {
+
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.todo_id;
+
+  // Grab data from http request
+  var data = {text: req.body.text, complete: req.body.complete};
+
+  // Get a connection client from the connection pool
+  pg.connect(connectionString, function(err, client, done){
+
+    // SQL query > Update Data
+    client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
+
+    // SQL query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    // Handle errors
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+router.delete('/api/v1/todos/:todo_id', function(req, res) {
+
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.todo_id;
+
+  // Get a connection client from the connection pool
+  pg.connect(connectionString, function(err, client, done){
+
+    // SQL query > Delete Data
+    client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+    // SQL query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    // Handle errors
+    if (err) {
+      console.log(err);
+    }
+  });
+});
