a50cc9dd5e1874d1a683c74dfed4ee7afcac29bf
Marco Lau <lau.career@gmail.com>
2015-06-22 02:15:28 -0400

    Add angular views

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost/todo';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 module.exports = router;
