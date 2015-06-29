edfd315ff3a99d989ddd7c823de040bb391782c6
Marco Lau <lau.career@gmail.com>
2015-06-28 23:36:52 -0400

    Add passport

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
